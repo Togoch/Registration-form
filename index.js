0528c4a75093578a4e108991c6a826ff99989bcb
Togoch <edwintogoch@gmail.com>
2024-08-08 00:17:47 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+//console.log("hello togoch");
+//console.log("i like pizza");
+//window.alert("i like ugali")
+//this is a comment
+// varibles are containers for holding data
+// let, var, const
+const $name = 30;
+console.log ($name)
+//function is a piece of a code that  you can call anywhere in your application
+//function nameof function(){}
+function hello (){
+    console.log ("hellow world")
+}
+hello();
+
+function greet(){
+let fn ='Edu';
+let sn = 'Togoch';
+console.log("goodmorning fn")
+}
+greet();
+
+//arrow function
+const fn =()=>{
+    console.log ("this is an arrow funtion")
+}
+fn()
+// objects are collection of properties
+//name:value
+let obj = {
+   name: "Togoch"
+   age : 22,
+   city : "Arusha"
+
+}
