8b40d4291db8e59c494ffe54da2b8a7c2a959921
Togoch <edwintogoch@gmail.com>
2024-08-23 23:12:11 +0300

    Add files via upload

---
+++ Project/script.js
@@ -0,0 +1,9 @@
+document.getElementById('signup-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('You have subscribed!');
+});
+
+document.getElementById('register-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    alert('Form submitted!');
+});
